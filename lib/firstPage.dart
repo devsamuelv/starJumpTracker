c1f8fd4042e6f944ae3ff7bded360a5d69e7e182
DevSamuelV <realalloutmusic@gmail.com>
2020-02-19 21:33:00 -0500

    navBarWorking

---
+++ lib/firstPage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(FirstPage());
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstHomePage(),
+    );
+  }
+}
+
+class FirstHomePage extends StatefulWidget {
+  @override
+  FirstHomePageState createState() => FirstHomePageState();
+}
+
+class FirstHomePageState extends State<FirstHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Icon(Icons.star_border, size: 150.0, color: Colors.pinkAccent),
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:starJumpTracker/secondPage.dart';
+import './firstPage.dart' as firstPage;
 
 void main() => runApp(MyApp());
 
@@ -10,15 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Star Jump Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see theq
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Star Jump Tracker'),
@@ -28,36 +20,13 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -66,41 +35,37 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
+    // super.initState();
+    TabController controller = new TabController(length: 2, vsync: this);
+
+    @override
+    void dispose() {
+      controller.dispose();
+      super.dispose();
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const SizedBox(height: 30),
-          RaisedButton(
-            onPressed: () {
-              // code here
-            },
-            textColor: Colors.white,
-            padding: const EdgeInsets.all(0.0),
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: <Color>[
-                    Color(0xFF0D47A1),
-                    Color(0xFF1976D2),
-                    Color(0xFF42A5F5),
-                  ],
-                ),
-              ),
-              padding: const EdgeInsets.all(10.0),
-              child:
-                  const Text('Gradient Button', style: TextStyle(fontSize: 20)),
-            ),
-          ),
-        ],
-      ),
-      ),
-    );
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        bottomNavigationBar: new Material(
+          color: Colors.blueAccent,
+          child: new TabBar(
+          controller: controller,
+          tabs: <Tab>[
+            new Tab(icon: Icon(Icons.dashboard)),
+            new Tab(icon: Icon(Icons.assignment_ind))
+          ],
+        )),
+        body: new TabBarView(
+          controller: controller,
+          children: <Widget>[
+            new firstPage.FirstHomePage(),
+            new secondPage(),
+          ],
+        ));
   }
 }
